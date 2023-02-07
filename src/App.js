1c5da5ec4a90763494d680f882a5f2912a54a539
Chris Rainey <chrainey@hotmail.co.uk>
2023-02-07 14:27:53 +0000

    Completed Exercise

--- src/App.js
+++ src/App.js
@@ -2,7 +2,29 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const App = () => {
-  return <h1>Hello World</h1>
+
+  const [ lines, setLines ] = useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await axios('https://api.tfl.gov.uk/line/mode/tube/status')
+      setLines(data)
+    }
+    getData()
+  })
+  return (
+    <section className='row'>
+      { lines.map(line => {
+        const { name, id } = line
+        return (
+          <div key={id} className={name}>
+            <h2>{name}</h2>
+            <h3>{line.lineStatuses[0].statusSeverityDescription}</h3>
+          </div>
+        )
+      })}
+    </section>
+  )
 }
 
 export default App
